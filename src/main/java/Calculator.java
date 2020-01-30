339867164a4f627c966c634daa02b07251ba31a3
conork033 <conork033@gmail.com>
2020-01-30 12:16:56 +0000

    calc

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,14 @@
 
 //this method will add two numbers
 public class Calculator {
+
+    public static void main(String[]args)
+
+    {
+        int one = 1;
+        int two = 2;
+        int sum = one + two;
+
+       System.out.println("sum: " + sum);
+    }
 }
